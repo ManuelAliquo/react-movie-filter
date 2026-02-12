26efe191a32dc8d0e5510e67436787e70d675c0f
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-12 16:54:51 +0100

    useEffect to filter movies based on selected option

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import movies from "./assets/data/movies.js";
 
 // salvo tutti i generi presenti nell'array movies
@@ -8,17 +8,26 @@ movies.forEach((movie) => {
 });
 
 export default function App() {
-  const [selectGenre, setSelectGenre] = useState("");
+  const [filterGenre, setFilterGenre] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
 
   // select change
-  const handleSelect = (e) => setSelectGenre(e.target.value);
+  const handleSelect = (e) => setFilterGenre(e.target.value);
+
+  // vedo se l'oggetto movie contiene l'elemento genre selezionato nella select
+  useEffect(() => {
+    setFilteredMovies(movies.filter((movie) => movie.genre.includes(filterGenre)));
+  }, [filterGenre]);
 
   return (
     <>
       <h1 className="text-center mt-4">Scegli il tuo Film</h1>
       <div className="container d-flex flex-column align-items-center">
-        <ul className="d-flex list-group my-5 w-75">
-          {movies.map((movie, index) => (
+        <div className="w-75 test-start mt-5">
+          <h4>Films</h4>
+        </div>
+        <ul className="d-flex list-group mb-5 w-75">
+          {filteredMovies.map((movie, index) => (
             <li className="list-group-item" key={index}>
               {movie.title}
             </li>
